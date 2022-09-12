a8876c12637c3f86cde6f0b4159e954ad51f1787
yusri <it21068850@my.sliit.lk>
2022-09-12 19:47:28 +0530

    added edittext

--- app/src/main/java/com/example/onlinegrocerystore/MainActivity.java
+++ app/src/main/java/com/example/onlinegrocerystore/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.onlinegrocerystore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
