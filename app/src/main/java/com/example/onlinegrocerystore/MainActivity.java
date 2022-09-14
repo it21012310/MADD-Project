4883bb8ed3fcc1d2199e72f043eefecea731c392
yusri <it21068850@my.sliit.lk>
2022-09-14 23:11:01 +0530

    added a new fragment, added a box and a a user icon

--- app/src/main/java/com/example/onlinegrocerystore/MainActivity.java
+++ app/src/main/java/com/example/onlinegrocerystore/MainActivity.java
@@ -2,16 +2,28 @@ package com.example.onlinegrocerystore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<ActivityMainBinding> extends AppCompatActivity {
 
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
